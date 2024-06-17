9018fa557111c53118ee1a279b257e1ef4b81580
Eldrin Dimabuyu <eldrin.dimabuyu368@gmail.com>
2024-06-17 15:34:55 +0100

    Added most rules to solhint config file and made it so that the config file is taken from the plugin directory rather than the working directory making the plugin more beginner friendly.

--- src/audit.js
+++ src/audit.js
@@ -1,17 +1,19 @@
 const { exec } = require("child_process");
+const path = require("path");
 
-function runSolhint(filePath) {
+async function runSolhint(file) {
+  const solhintConfigPath = path.join(__dirname, '..', '.solhint.json'); // Looks for .solhint config file inside plugin directory rather than working directory
+  const command = `yarn solhint --config ${solhintConfigPath} ${file}`;
+  console.log(`Running command: ${command}`); // Debug logging
+
   return new Promise((resolve, reject) => {
-    exec(`yarn solhint ${filePath}`, (error, stdout, stderr) => {
+    exec(command, (error, stdout, stderr) => {
       if (error) {
-        reject(error);
-        return;
+        console.error(`Error details: ${stderr}`); // Detailed error logging
+        reject(`Error running Solhint: ${stderr}`);
+      } else {
+        resolve(stdout);
       }
-      if (stderr) {
-        reject(new Error(stderr));
-        return;
-      }
-      resolve(stdout);
     });
   });
 }
